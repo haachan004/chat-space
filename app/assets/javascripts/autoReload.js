1d35f8ab8d6bff4a97e09251ddefa498b10a5e85
haachan004 <yuzuha.okamoto@di-v.co.jp>
2020-06-01 10:22:03 +0900

    js edit

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -1,76 +1,76 @@
-$(function(){
+// $(function(){
 
-  function buildHTML(message){
-    if ( message.image ) {
-      let html =
-        `<div class="mainchat__chatScreen__Box" data-message-id=${message.id}>
-          <div class="mainchat__chatScreen__Box__infoBox">
-            <div class="mainchat__chatScreen__Box__infoBox__name">
-              ${message.user_name}
-            </div>
-            <div class="mainchat__chatScreen__Box__infoBox__timestamp">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="mainchat__chatScreen__Box__infoBox__subTitle">
-            <p class="Message__content">
-              ${message.content}
-            </p>
-            <img class="Message__image" src="${message.image}">
-          </div>
-        </div>`
-      return html;
-    } else {
-      let html =
-      `<div class="mainchat__chatScreen__Box" data-message-id=${message.id}>
-        <div class="mainchat__chatScreen__Box__infoBox">
-          <div class="mainchat__chatScreen__Box__infoBox__name">
-            ${message.user_name}
-          </div>
-          <div class="mainchat__chatScreen__Box__infoBox__timestamp">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="mainchat__chatScreen__Box__infoBox__subTitle">
-          <p class="Message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-      return html;
-    };
-  }
+//   function buildHTML(message){
+//     if ( message.image ) {
+//       let html =
+//         `<div class="mainchat__chatScreen__Box" data-message-id=${message.id}>
+//           <div class="mainchat__chatScreen__Box__infoBox">
+//             <div class="mainchat__chatScreen__Box__infoBox__name">
+//               ${message.user_name}
+//             </div>
+//             <div class="mainchat__chatScreen__Box__infoBox__timestamp">
+//               ${message.created_at}
+//             </div>
+//           </div>
+//           <div class="mainchat__chatScreen__Box__infoBox__subTitle">
+//             <p class="Message__content">
+//               ${message.content}
+//             </p>
+//             <img class="Message__image" src="${message.image}">
+//           </div>
+//         </div>`
+//       return html;
+//     } else {
+//       let html =
+//       `<div class="mainchat__chatScreen__Box" data-message-id=${message.id}>
+//         <div class="mainchat__chatScreen__Box__infoBox">
+//           <div class="mainchat__chatScreen__Box__infoBox__name">
+//             ${message.user_name}
+//           </div>
+//           <div class="mainchat__chatScreen__Box__infoBox__timestamp">
+//             ${message.created_at}
+//           </div>
+//         </div>
+//         <div class="mainchat__chatScreen__Box__infoBox__subTitle">
+//           <p class="Message__content">
+//             ${message.content}
+//           </p>
+//         </div>
+//       </div>`
+//       return html;
+//     };
+//   }
 
-  let reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.mainchat__chatScreen__Box:last').data("message-id");
+//   let reloadMessages = function() {
+//     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+//     let last_message_id = $('.mainchat__chatScreen__Box:last').data("message-id");
     
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
-      if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
-        let insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.mainchat__chatScreen').append(insertHTML);
-        $('.mainchat__chatScreen').animate({ scrollTop: $('.mainchat__chatScreen')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  };
-  setInterval(reloadMessages, 7000);
-});
+//     $.ajax({
+//       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+//       url: "api/messages",
+//       //ルーティングで設定した通りhttpメソッドをgetに指定
+//       type: 'get',
+//       dataType: 'json',
+//       //dataオプションでリクエストに値を含める
+//       data: {id: last_message_id}
+//     })
+//     .done(function(messages) {
+//       // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+//       if (messages.length !== 0) {
+//         //追加するHTMLの入れ物を作る
+//         let insertHTML = '';
+//         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+//         $.each(messages, function(i, message) {
+//           insertHTML += buildHTML(message)
+//         });
+//         //メッセージが入ったHTMLに、入れ物ごと追加
+//         $('.mainchat__chatScreen').append(insertHTML);
+//         $('.mainchat__chatScreen').animate({ scrollTop: $('.mainchat__chatScreen')[0].scrollHeight});
+//       }
+//     })
+//     .fail(function() {
+//       alert('error');
+//     });
+//   };
+//   setInterval(reloadMessages, 7000);
+// });
