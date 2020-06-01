585cebff390907ea9bf494a77b5bcea75174a9a9
haachan004 <yuzuha.okamoto@di-v.co.jp>
2020-06-01 09:54:27 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.14.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'Chat-space'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:haachan004/chat-space.git'
